7f1dbcff6d011b427f3081a923cf7c369481656e
Mano Mambane <manomambane05@gmail.com>
2021-08-20 14:15:20 +0200

    Graph

---
+++ Matplotlib Poll/figures_axes_plot.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+
+plt.figure()
+plt.axis([0, 6, 0, 20])
+plt.show()
--- Matplotlib Poll/graph.py
+++ Matplotlib Poll/graph.py
@@ -1,5 +1,10 @@
 import matplotlib.pyplot as plt
 
 plt.figure()
-plt.plot([1, 2, 3, 4], [3, 5, 9, 25])
+
+plt.xlabel("Categories")
+plt.ylabel("Amounts")
+plt.title("Categories vs. Amounts")
+
+lines = plt.plot(["Men", "Women", "Children"], [3, 5, 9])
 plt.show()
