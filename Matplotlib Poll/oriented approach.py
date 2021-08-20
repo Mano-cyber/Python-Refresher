3ba7ab1901611afcab6ce80d5524abaab805ea57
Mano Mambane <manomambane05@gmail.com>
2021-08-20 14:23:16 +0200

    Graph

---
+++ Matplotlib Poll/oriented approach.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+figure = plt.figure()
+axes = figure.add_subplot()
+axes.set_title("Test line")
+axes.set_xlabel("Numbers")
+axes.set_ylabel("Count")
+axes.plot([1, 2, 3, 4], [3, 5, 9, 25])
+show = plt.show()
+show()
